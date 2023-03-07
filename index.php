cbd995d637b65e7bbf6902d090cc40b0249ebb63
sauzuniga <88364351+sauzuniga@users.noreply.github.com>
2023-03-06 21:29:43 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//echo phpinfo();
+$variable ="Programacion computacional IV";
+$entero = 20;
+var_dump($variable);
+//casting 
+$variable=(bool) $variable;
+var_dump($variable);
+$a=null;
+$b="";
+$c="Valor";
+echo $z ?? $a ?? $b ?? $c;
+//operador nave espacial
+echo 6<=>20;
+?>
